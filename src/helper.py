021532a097bb4a8d73cee72fd47a0cf58c7f0c1f
Shuvo <suvo10.info@gmail.com>
2026-05-25 18:31:54 +0600

    fkmnlk

--- src/helper.py
+++ src/helper.py
@@ -23,4 +23,15 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
         src = doc.metadata.get("source", "")
         minimal_docs.append(Document(page_content=doc.page_content, metadata={"source": src}))
     return minimal_docs
+
+## text splitter to split the documents into smaller chunks to fit into the context window of the language model
     
+    
+def text_split(minimal_docs):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
+    
+    texts_chunk=text_splitter.split_documents(minimal_docs)
+    return texts_chunk
+
+
+

f3ff4a52abcbbe57b95abf8d33f0b70f118e264d
Shuvo <suvo10.info@gmail.com>
2026-05-25 18:29:43 +0600

    v m vm,.

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,18 @@
-def main():
-    pass 
 
-if __name__ == "__main__":
-    main()
-    
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter   
+from typing import List
+from langchain.schema import Document
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(data, glob="**/*.pdf", loader_cls=PyPDFLoader, show_progress=True)
+    documents = loader.load()
+    return documents
+
+
+
     

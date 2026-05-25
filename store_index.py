27f42e334555b38b04ed50033ca7b167ccd7deb0
Shuvo <suvo10.info@gmail.com>
2026-05-25 18:26:32 +0600

    jnxklm

---
+++ store_index.py
@@ -0,0 +1,12 @@
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+import os 
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
+
+

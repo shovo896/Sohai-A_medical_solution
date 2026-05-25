4892cab76443af0d23384ebb220f8c07bc9e270d
Shuvo <suvo10.info@gmail.com>
2026-05-25 18:46:05 +0600

    ikkk

--- store_index.py
+++ store_index.py
@@ -23,6 +23,22 @@ texts_chunk=text_split(filter_data)
 embeddings=download_embeddings()
 # initialize pinecone client
 
+pinecone_api_key=PINECONE_API_KEY
+pinecone = Pinecone(api_key=pinecone_api_key) 
+
+
+index_name = "medical-chatbot"
+pinecone_cloud = os.getenv("PINECONE_CLOUD", "aws")
+pinecone_region = os.getenv("PINECONE_REGION", "us-east-1")
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud=pinecone_cloud, region=pinecone_region),
+    )
+index = pc.Index(index_name)
 
 
 

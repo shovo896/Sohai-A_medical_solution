04ecf4a1cba27888a4b4395c20485cfc6df24687
Shuvo <suvo10.info@gmail.com>
2026-05-25 20:23:13 +0600

    l;,l,;

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, render_template
+from src.helper import download_huggingface_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+from src.prompt import *

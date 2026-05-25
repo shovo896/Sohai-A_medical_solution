af4715e82efb9316f495d69f47e3a1b574487f8e
Shuvo <suvo10.info@gmail.com>
2026-05-25 20:38:44 +0600

    dk

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from src.helper import download_huggingface_embeddings
+from src.helper import download_embeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
@@ -18,6 +18,11 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+
+embeddings=download_embeddings()
+index_name = "medical-chatbot"
+docsearch=PineconeVectorStore(embedding_function=embeddings, index_name=index_name)
 
 
 

9817d185730195044d47708902c89e7bfb2e0ce4
Shuvo <suvo10.info@gmail.com>
2026-05-25 12:26:54 +0600

    cnx ,

---
+++ template.sh
@@ -0,0 +1,18 @@
+#creating directory for the 
+
+mkdir -p  src 
+
+
+
+#cretaing files for the project
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env 
+touch setup.py 
+touch app.py 
+touch store_index.py 
+
+echo " Directory and files created successfully!"
